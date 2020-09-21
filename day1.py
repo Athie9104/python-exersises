b0070c7ffb0767e7de4d92128b4e3fc989e78ace
Athie9104 <athenkosigongotha7@gmail.com>
2020-09-21 16:36:03 +0200

    Changes.py

--- day1.py
+++ day1.py
@@ -7,3 +7,9 @@ print("I was coded in " + class_name)
 class_students = "20"
 
 print("There are " + class_students + "students in " + class_name)
+
+# Task 1. Ask for name
+student_name=input("What is your name: \n")
+print("So your name is " + student_name)
+
+# Task 2. Print circumferance of a circle
---
+++ day1exercise.py
@@ -0,0 +1,11 @@
+# Task 1 
+f_name = input("Your Name\n")
+H = "Hello "
+_H = " How are you?"
+print(H + f_name + _H)
+
+# Task 2
+_radius = int(input("Enter Radius\n"))
+_cir = 2*_radius*3.1415
+print(_cir)
+
