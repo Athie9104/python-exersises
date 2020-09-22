04c13838536254cf5ab2570d8218d9de6c38f7bc
Athie9104 <athenkosigongotha7@gmail.com>
2020-09-22 12:44:43 +0200

    day2_Changes

--- day2_exercise.py
+++ day2_exercise.py
@@ -1,4 +1,6 @@
 name1=input("Enter your name: \n")
+#result=len(name1)
 F_letter=name1[0]
-L_letter=name1[4]
+L_letter=name1[-1]
 print("Hi I am "+name1,"\n","First letter is "+F_letter,"\n","Last letter is "+L_letter)
+#print(result)
